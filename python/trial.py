05bf8c981c1f640b4312bc9ab5590953bc2a1931
Goutham S Kunnel <93504229+GouthamSKunnel@users.noreply.github.com>
2021-11-17 14:25:10 +0530

    Add files via upload

---
+++ python/trial.py
@@ -0,0 +1,3 @@
+a,b=int(input("enter 2 nos")),int(input())
+print(a,b)
+print (type(a))
---
+++ python/vartodollar.py
@@ -0,0 +1,8 @@
+str=input("Enter a string ")
+l=len(str)
+var=str[0]
+for i in range(l):
+    print(str.index(i))
+    
+
+
