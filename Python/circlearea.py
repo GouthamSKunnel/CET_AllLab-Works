b3c29d697ab7171a8493ea8bcf09e960d12d108d
Goutham S Kunnel <93504229+GouthamSKunnel@users.noreply.github.com>
2021-11-17 14:25:30 +0530

    Delete Python directory

--- Python/circlearea.py
+++
@@ -1,5 +0,0 @@
-print("------------Area of circle-------------")
-radius=int(input("enter the radius of the circle "))
-import math
-print("area of circle=",(math.pi*(radius*radius)))
-print("perimeter = ",(2*math.pi*radius))
--- Python/stringvarreplace.py
+++
@@ -1,7 +0,0 @@
-str=input("Enter the string ")
-str=list(str)
-ch=str[0]
-str[0]=str[-1]
-str[-1]=ch
-str1=' '.join(str)
-print(str1)
